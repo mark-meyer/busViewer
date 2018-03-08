4acf59d18eaab27943204aac85ec4794d3bb0004
Mark Meyer <mark@photo-mark.com>
2018-03-07 17:18:42 -0900

    add express directory

--- express/app.js
+++ express/app.js
@@ -26,7 +26,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 app.use('/', index);
 app.use('/api', api);
